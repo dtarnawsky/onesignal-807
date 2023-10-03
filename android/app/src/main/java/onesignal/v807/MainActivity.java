2dcd689daf969bfb17e7b40a8ee273d21e9ba3e8
damian.ionic <damian@ionic.io>
2023-10-03 08:10:28 -0700

    initial commit

---
+++ android/app/src/main/java/onesignal/v807/MainActivity.java
@@ -0,0 +1,5 @@
+package onesignal.v807;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'onesignal.v807',
+  appName: 'onesignal-807',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
